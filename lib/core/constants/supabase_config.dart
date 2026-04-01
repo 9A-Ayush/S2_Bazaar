e453abe164b06fbcd3075c534703e471a15028df
Ayush Kumar <wemayush@gmail.com>
2026-04-01 18:53:21 +0530

    Delete lib/core/constants/supabase_config.dart

--- lib/core/constants/supabase_config.dart
+++
@@ -1,5 +0,0 @@
-abstract class SupabaseConfig {
-  static const String url = 'https://ndbqlgftihujnonqejnt.supabase.co';
-  static const String anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5kYnFsZ2Z0aWh1am5vbnFlam50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxNzgxNzksImV4cCI6MjA4OTc1NDE3OX0.KLL74ISEYBf4dhSALViiX8L6A7sx1Z2ouVZsub0Kjos';
-}
